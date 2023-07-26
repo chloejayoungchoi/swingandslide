47c62836c56579ceb0afc51862bdf9fc8ce4f7a0
chloejayoungchoi <cchoi6484@gmail.com>
2023-07-26 12:18:41 -0700

    recaptcha for email form

--- src/Contact.js
+++ src/Contact.js
@@ -1,19 +1,43 @@
 import React, { useRef } from "react";
 import emailjs from '@emailjs/browser';
+import ReCAPTCHA from "react-google-recaptcha";
 
 function Contact() {
     const form = useRef();
+    const recaptchaRef = React.createRef();
 
     function sendEmail(e) {
         e.preventDefault();
+
+        // recaptcha
+        const recaptchaValue = recaptchaRef.current.getValue();
+        
         console.log('sending email soon');
-        console.log(form.current)
-        emailjs.sendForm('service_cchoi6484', 'template_5c5n5td', form.current, 'JpXUUhatC2LSUog0c')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
+
+        let emailForm = {
+            "playground_name" : form.current.playground_name.value,
+            "message" : form.current.message.value,
+            "user_email" : form.current.user_email.value,
+            "g-recaptcha-response" : recaptchaValue
+        };
+        console.log(emailForm);
+        emailjs.send('service_cchoi6484', 'template_5c5n5td', emailForm, 'JpXUUhatC2LSUog0c')
+            .then(function(response) {
+                console.log('SUCCESS!', response.status, response.text);
+            }, function(error) {
+                console.log('FAILED...', error);
             });
+
+        // emailjs.sendForm('service_cchoi6484', 'template_5c5n5td', emailForm, 'JpXUUhatC2LSUog0c')
+        //     .then((result) => {
+        //         console.log(result.text);
+        //     }, (error) => {
+        //         console.log(error.text);
+        //     });
+    }
+
+    function onChange(value) {
+        console.log("Captcha value:", value);
     }
 
     return (
@@ -22,9 +46,9 @@ function Contact() {
                 <div className="card-body pt-0">
                     <div className="d-flex justify-content-between align-items-center px-2 mb-2">
                         <div>
-                            <h3 className="fs-2 mb-0 me-3">Email Us</h3>
-                            <span className="text-muted">at cchoi6484@gmail.com</span><br/>
-                            <span className="text-muted">or use the form below</span>
+                            <h3 className="fs-2 mb-0 me-3">Please Let Us Know</h3>
+                            {/* <span className="text-muted">at cchoi6484@gmail.com</span><br/>
+                            <span className="text-muted">or use the form below</span> */}
                         </div>
                         <img className="w-50" src="/images/email.jpg" alt="Email Us"></img>
                     </div>
@@ -37,9 +61,14 @@ function Contact() {
                         </ol>
                     </div>
                     <form ref={form} onSubmit={sendEmail}>
-                        <input type="text" className="form-control mb-2" name="user_name" placeholder="Your Name" />
+                        <input type="text" className="form-control mb-2" name="playground_name" placeholder="Playground Name" />
                         <textarea className="form-control mb-2" name="message" rows="5" placeholder="Message"></textarea>
                         <input type="text" className="form-control mb-2" name="user_email" placeholder="Your Email Address" />
+                        <ReCAPTCHA
+                            sitekey="6LfuslgnAAAAAJeTJGY5KhGUcHehfnLPheVv4_bC"
+                            ref={recaptchaRef}
+                            onChange={onChange}
+                        />
                         <button type="submit" className="btn btn-primary w-100">
                             Send
                             <span className="visually-hidden">Send an email</span>
