fe3f8b85083c9988110ad901b68f3c7f86e57d3d
chloejayoungchoi <cchoi6484@gmail.com>
2023-08-22 08:28:42 -0700

    안드로이드 하드웨어 백버튼 클릭시 갤러리 클로즈

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -16,11 +16,39 @@ function Gallery(props) {
       pswpModule: () => import('photoswipe'),
     });
     lightbox.init();
-    if(props.imageList.length > 0 && props.imageList[0].filename !== 'thumb-default.jpg') ref.current.click()
+    if(props.imageList.length > 0 && props.imageList[0].filename !== 'thumb-default.jpg') {
+      ref.current.click();
+    }
+
+    lightbox.on('beforeOpen', () => {
+      if (window.ReactNativeWebView) {
+        window.ReactNativeWebView.postMessage(JSON.stringify({ type: "GALLERY", value: true }));
+      } 
+    });
+
+    lightbox.on('close', () => {
+      if (window.ReactNativeWebView) {
+        window.ReactNativeWebView.postMessage(JSON.stringify({ type: "GALLERY", value: false }));
+      } 
+    });
+
+    function closeGallery(e) {
+      try{
+        let msg = JSON.parse(e.data);
+        if(msg.type === "CALL" && msg.value === "CLOSE_GALLERY")
+          lightbox.destroy();
+      }catch(e) {
+        console.error(e)
+      }
+    }
+
+    document.addEventListener('message', closeGallery);
 
     return () => {
       lightbox.destroy();
       lightbox = null;
+
+      document.removeEventListener("message", closeGallery);
     };
   }, [props.imageList]);
 
