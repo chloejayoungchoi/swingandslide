ef9a7ad7b5eb5d35a1b57e8b33574b9e6eafa317
chloejayoungchoi <cchoi6484@gmail.com>
2023-08-29 06:13:37 -0700

    loading image size fixed

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,7 +4,7 @@ function Loading(p) {
 
     return (
         <div className="loading-container">
-            <img src="images/loading.gif" style={{width:'30vw'}}/>
+            <img src="images/loading.gif" style={{width:'80px'}}/>
         </div>
     );
 }
